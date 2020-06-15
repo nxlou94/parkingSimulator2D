2e0e6532cf3267ed7a4d68c9548ec1682e91a1b0
nxlou94 <nxlou94@126.com>
2020-06-15 14:55:06 +0800

    appearance change

--- car.js
+++ car.js
@@ -1,113 +1,4 @@
-function rect(x, y, w, h, alpha){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.alpha = alpha;
-    this.vertex = [];
-    this.vertex.push([x, y]);
-    this.vertex.push([x + w * Math.cos(alpha), y + w * Math.sin(alpha)]);
-    this.vertex.push([x + w * Math.cos(alpha) - h * Math.sin(alpha), y + w * Math.sin(alpha) + h * Math.cos(alpha)]);
-    this.vertex.push([x - h * Math.sin(alpha), y + h * Math.cos(alpha)]);
-    this.edge = [];
-    this.edge.push([this.vertex[1][0] - this.vertex[0][0], this.vertex[1][1] - this.vertex[0][1]]);
-    this.edge.push([this.vertex[2][0] - this.vertex[1][0], this.vertex[2][1] - this.vertex[1][1]]);
-    this.xMax = Math.max(this.vertex.map(v => v[0]));
-    this.xMin = Math.min(this.vertex.map(v => v[0]));
-    this.yMax = Math.max(this.vertex.map(v => v[1]));
-    this.yMin = Math.min(this.vertex.map(v => v[1]));
-    this.setColor = function(color){
-        this.color = color;
-    }
-    this.stroke = function(){
-        ctx.save();
-        ctx.strokeStyle = this.color;
-        ctx.lineWidth = 1;
-        ctx.strokeRect(this.vertex[0][0], this.vertex[0][1], 2, 2);
-        ctx.strokeRect(this.vertex[3][0], this.vertex[3][1], 2, 2);
-        ctx.strokeRect(this.vertex[1][0], this.vertex[1][1], 2, 2);
-        ctx.strokeRect(this.vertex[2][0], this.vertex[2][1], 2, 2);
-        ctx.restore();
-    };
-    this.draw = function(){
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.alpha);
-        ctx.strokeStyle = this.color;
-        ctx.lineWidth = 1;
-        ctx.strokeRect(0, 0, this.w, this.h);
-        ctx.restore();
-        //console.log(this.vertex);
-    };
-};
-
-function rectCollision(rect1, rect2){
-    if(rect1.xMax < rect2.xMin || rect1.xMin > rect2.xMax || rect1.yMax < rect2.yMin || rect1.yMin > rect2.yMax){
-        return false;
-    }else{
-        return rectNarrowCollision(rect1, rect2);
-    }
-}
-
-function rectContain(rect1, rect2){
-    var xMin = Infinity;
-    var xMax = -Infinity;
-    var yMin = Infinity;
-    var yMax = -Infinity;
-    const x2Normalized = [Math.cos(rect2.alpha), Math.sin(rect2.alpha)];
-    const x2Projection = dot([rect2.x, rect2.y], x2Normalized);
-    const y2Normalized = [-Math.sin(rect2.alpha), Math.cos(rect2.alpha)];
-    const y2Projection = dot([rect2.x, rect2.y], y2Normalized);
-    return rect1.vertex.every(vertex => {
-        var x = dot(vertex, x2Normalized) - x2Projection;
-        var y = dot(vertex, y2Normalized) - y2Projection;
-        return (x >= 0 && x <= rect2.w && y >= 0 && y <= rect2.h);
-    });
-}
-
-function rectNarrowCollision(rect1, rect2){
-    edgeNormal1 = [normal(rect1.edge[0]), normal(rect1.edge[1])];
-    edgeNormal2 = [normal(rect2.edge[0]), normal(rect2.edge[1])];
-    free1 = edgeNormal1.some(n => {
-        let r1min = Infinity;
-        let r1max = -Infinity;
-        rect1.vertex.forEach(v => {
-            r1min = Math.min(dot(v, n), r1min);
-            r1max = Math.max(dot(v, n), r1max);
-        });
-        let r2min = Infinity;
-        let r2max = -Infinity;
-        rect2.vertex.forEach(v => {
-            r2min = Math.min(dot(v, n), r2min);
-            r2max = Math.max(dot(v, n), r2max);
-        });
-        if(r1min > r2max || r1max < r2min){
-            return true;
-        }else{
-            return false;
-        }
-    });
-    free2 = edgeNormal2.some(n => {
-        let r1min = Infinity;
-        let r1max = -Infinity;
-        rect1.vertex.forEach(v => {
-            r1min = Math.min(dot(v, n), r1min);
-            r1max = Math.max(dot(v, n), r1max);
-        });
-        let r2min = Infinity;
-        let r2max = -Infinity;
-        rect2.vertex.forEach(v => {
-            r2min = Math.min(dot(v, n), r2min);
-            r2max = Math.max(dot(v, n), r2max);
-        });
-        if(r1min > r2max || r1max < r2min){
-            return true;
-        }else{
-            return false;
-        }
-    });
-    return !(free1 || free2);
-}
+bgColor = '#a1a1a1';
 
 var obsList = [];
 function makeObs(x, y, w, h, alpha){
@@ -116,7 +7,7 @@ function makeObs(x, y, w, h, alpha){
 
 function drawObs(){
     obsList.forEach(obs => {
-        obs.draw();
+        obs.draw(ctx);
         //obs.stroke();
     })
 }
@@ -216,7 +107,7 @@ function car(length = 80, width = 36, frontWheelPos = 16, rearWheelPos = 64, fro
     new wheel(this.xFrontWheel, this.yFrontWheel, true),
     new wheel(-this.xRearWheel, this.yRearWheel, false), 
     new wheel(this.xRearWheel, this.yRearWheel, false)];
-    this.setPos = function(x, y, theta, beta) {
+    this.setPos = function(x, y, theta, beta){
         this.x = x;
         this.y = y;
         this.theta = theta;
@@ -326,10 +217,12 @@ function car(length = 80, width = 36, frontWheelPos = 16, rearWheelPos = 64, fro
         this.rect = new rect(this.x + Math.sin(this.theta) * this.length / 2 - Math.cos(this.theta) * this.width / 2, 
         this.y  - Math.cos(this.theta) * this.length / 2 - Math.sin(this.theta) * this.width / 2, 
         this.width, this.length, this.theta);
+        this.rect.setColor(this.color);
     };
     this.draw = function(){
+        this.rect.draw(ctx);
         ctx.save();
-        ctx.fillStyle = color;
+        ctx.fillStyle = this.color;
         ctx.translate(this.x, this.y);
         ctx.rotate(this.theta);
         ctx.globalAlpha = 0.5;
@@ -348,24 +241,17 @@ function car(length = 80, width = 36, frontWheelPos = 16, rearWheelPos = 64, fro
         this.wheels.forEach(wheel => {
             if((wheel.steer && this.gear == 'D') || (!wheel.steer && this.gear == 'R')){
                 ctx.save();
-                ctx.lineWidth = 3;
-                //ctx.fillStyle = gradient;
-                ctx.strokeStyle = gradient;
+                ctx.beginPath();
                 let l = Math.min(Math.PI / 2, this.length / wheel.IRR);
                 if(this.currentSteer == 0){
                     if(this.gear == 'D'){
-                        ctx.beginPath();
                         ctx.moveTo(wheel.x, wheel.y);
                         ctx.lineTo(wheel.x, wheel.y + this.length);
-                        ctx.stroke();
                     }else{
-                        ctx.beginPath();
                         ctx.moveTo(wheel.x, wheel.y);
                         ctx.lineTo(wheel.x, wheel.y - this.length);
-                        ctx.stroke();
                     }
                 }else{
-                    ctx.beginPath();
                     let r, sAngle, eAngle, c;
                     if(wheel.IRR < 0){
                         r = -wheel.IRR;
@@ -383,27 +269,37 @@ function car(length = 80, width = 36, frontWheelPos = 16, rearWheelPos = 64, fro
                         eAngle += l;
                     }
                     ctx.arc(this.ICRL, this.yRearWheel, r, sAngle, eAngle, c);
-                    ctx.stroke();
                 }
+                /* ctx.lineWidth = 5;
+                ctx.strokeStyle = bgColor;
+                ctx.stroke(); */
+                ctx.lineWidth = 3;
+                ctx.strokeStyle = gradient;
+                ctx.stroke();
                 ctx.restore();
             }
             if(wheel.steer && wheel.beta){
                 ctx.save();
                 ctx.translate(wheel.x, wheel.y);
                 ctx.rotate(wheel.beta);
+                /* ctx.fillStyle = bgColor;
+                ctx.fillRect(-3, -this.wheelRadius - 1, 6, 12);
+                ctx.fillStyle = this.color; */
                 ctx.fillRect(-2, -this.wheelRadius, 4, 10);
                 //ctx.fillText(Math.round(wheel.IRR), 0, 0);
                 ctx.restore();
             } else {
+                /* ctx.fillStyle = bgColor;
+                ctx.fillRect(wheel.x - 3, wheel.y - this.wheelRadius - 1, 6, 12);
+                ctx.fillStyle = this.color; */
                 ctx.fillRect(wheel.x - 2, wheel.y - this.wheelRadius, 4, 10);
                 //ctx.fillText(Math.round(wheel.IRR), wheel.x, wheel.y);
             }
         });
         ctx.restore();
-        this.rect.draw();
         //ctx.fillRect(this.x + this.ICRL * Math.cos(this.theta) - 1, this.y + this.ICR * Math.sin(this.theta) - 1, 2, 2);
     };
-    this.drawStats = function () {
+    this.drawStats = function(){
         ctx.strokeText('Speed: ' + this.speed, 10, 20);
         ctx.strokeText('Steer: ' + this.currentSteer, 10, 50);
         ctx.strokeText('- ' + this.gear + ' -', 10, 35);
@@ -456,7 +352,7 @@ function update(){
     ctx.rotate(-myCar.theta + Math.PI);
     ctx.translate(-myCar.x, -myCar.y);
     inTarget();
-    target.draw();
+    target.draw(ctx);
     myCar.draw();
     obsCollision();
     drawObs();
